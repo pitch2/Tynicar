aa834dbbb947a022f7e46b9886f3bf2abd744f65
pitch <95540893+pitch2@users.noreply.github.com>
2024-03-08 11:52:02 +0100

    Update question_3.py

--- mission_2/question_3.py
+++ mission_2/question_3.py
@@ -1,8 +1,7 @@
 mdp_q = input("Saisir le mot de passe: ") #question du mdp
 mdp = "Padawan" #bon mdp
 
-
-def voiture():
+if mdp_q == mdp: #condition du bon mdp
     marque = input("Marque de la voiture: ")
     modèle = input("Modèle de la voiture: ")
     red = int(input("Saisir 1 si votre voiture est électrique, saisir 2 si votre voiture n'est pas électrique: "))
@@ -19,11 +18,6 @@ def voiture():
         prix_TTC = prix_TTC - (red/100) #reduction de - 10%
 
     print(f"Votre {modèle}, de la marque {marque}, coûte {prix_TTC} €") #affichage
-
-
-if mdp_q == mdp: #condition du bon mdp
-    voiture() #appelle de la fonction quand le mdp est correct
-    
     
 else:
     print("Mauvais mot de passe")#incorrect
